faddf105967f21a4ef8ea76667ac8ee2a0338ab5
SakhnoDima <134371251+SakhnoDima@users.noreply.github.com>
2023-11-16 11:38:20 +0200

    Update PrivateRoute.jsx

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useAllSelectors } from "../../hooks/useAllSelectors";
 import { Navigate } from "react-router-dom";
 
-const PrivateRoute = ({ children, redirectTo = "/auth" }) => {
+const PrivateRoute = ({ children, redirectTo = "auth" }) => {
   const { isLoggedIn } = useAllSelectors();
   return isLoggedIn ? children : <Navigate to={redirectTo} />;
 };
