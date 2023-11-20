a6f04e09bfac07573f63ec7169e62c794eb602da
SakhnoDima <134371251+SakhnoDima@users.noreply.github.com>
2023-11-20 10:55:00 +0200

    bug fix

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -4,9 +4,9 @@ const Container = ({ children, cls = "" }) => {
   return (
     <section
       className={`
-        mx-auto w-[320px] px-5 laptop:w-[768px] desktop:px-[124px] desktop:w-[100%] ${
+        mx-auto min-w-[320px] px-5 laptop:w-[768px] desktop:px-[124px] desktop:w-[100%] ${
           cls === "material"
-            ? "min-w-[320px] w-[100%] bg-mob bg-no-repeat bg-right bg-cover retina:bg-mob2x laptop:bg-tab laptop:retina:bg-tab2x laptop:px-[32px] desktop:bg-desk desktop:retina:bg-desk2x desktop:px-[123px]"
+            ? " w-[100%] bg-mob bg-no-repeat bg-right bg-cover retina:bg-mob2x laptop:bg-tab laptop:retina:bg-tab2x laptop:px-[32px] desktop:bg-desk desktop:retina:bg-desk2x desktop:px-[123px]"
             : ""
         }
   `}
