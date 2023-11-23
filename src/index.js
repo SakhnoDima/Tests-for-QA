4de1ff294ed56631e07efb86cc1806940b9a3a90
SakhnoDima <134371251+SakhnoDima@users.noreply.github.com>
2023-11-23 17:23:08 +0200

    3

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/Tests-for-QA">
+        <BrowserRouter>
           <App />
         </BrowserRouter>
       </PersistGate>
